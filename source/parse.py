9bdc5dac1abe6169a47554879cec796a22de14b3
Plamen Kolev <p.kolev22@gmail.com>
2019-08-10 17:29:03 +0100

    csv and json done with youtube url

--- source/parse.py
+++ source/parse.py
@@ -1,18 +1,36 @@
+import html
 import re
+import csv
+import urllib.parse
+import json
+
+class Genre:
+    pass
+
+class Artist:
+    pass
+
+class Row:
+    def __init__(self, uniqueId, genre, artist, song ,year, url):
+        self.uniqueId = uniqueId
+        self.genre = genre
+        self.artist = artist
+        self.song = song,
+        self.year = year
+        self.url = url
+
 
 file = open("data.list", "r", encoding="utf8")
 ids = []
 genres = []
 artists = []
 songs = []
+years = []
 
-sentence_regexp = "[\w+,&.'\-\(\)$!–„” ]"
 id_regexp = "^(\d+)\s\|\s(.*)"
-genre_regexp = "([\w -\]\[]+)\|\s(.*)"
-artist_regexp = "([\w .&,]+)-(.*)"
-# artists_regexp = ".+\|.+\|(\s%s+)" % sentence_regexp
-# songs_regexp = "^.+\|.+\|\s%s+-\s(.+)[(]?" % sentence_regexp
-
+genre_regexp = "^([\w -\]\[]+)\|\s(.*)"
+artist_regexp = "^([\w ”„+$!–.&,';\(\)]+)-(.*)"
+song_and_year_regexp = "^(.+)(\(.*\))"
 
 for line in file:
     idResult = re.search(id_regexp, line)
@@ -21,19 +39,54 @@ for line in file:
     ids.append(id)
 
     genreResult = re.search(genre_regexp, rest)
-    genre = genreResult[1]
-    genres.append(genre)
+
+    genres.append(genreResult[1])
     rest = genreResult[2]
 
     artistResult = re.search(artist_regexp, rest)
-    artist = artistResult[1]
+    artists.append(artistResult[1])
     rest = artistResult[2]
 
-    print(artist)
+    songAndYearResult = re.search(song_and_year_regexp, rest)
+    songs.append(songAndYearResult[1])
+    if (songAndYearResult[2]):
+        years.append(songAndYearResult[2])
+    else:
+        years.append("-1")
 
-    # genres.append(re.search(genres_regexp, line)[1])
-    # artists.append(re.search(artists_regexp, line)[1])
-    #
-    # song_and_year = re.search(songs_regexp, line)[1]
-    # print(songs_regexp)
-    # print(song_and_year)
+
+def sanitizeString(string):
+    return string.strip().rstrip()
+
+
+csvFile = csv.writer(open('data.csv', "w", encoding="utf8", newline=''))
+csvFile.writerow(["id", "genre", "artist", "song", "year", "youtube search"])
+for i in range(0, len(years)):
+    artist = sanitizeString(artists[i])
+    song = sanitizeString(songs[i])
+    songSearchTerm = urllib.parse.quote(html.escape("%s-%s" % (artist, song)))
+    csvFile.writerow([
+        int(ids[i]),
+        sanitizeString(genres[i]),
+        artist,
+        song,
+        sanitizeString(years[i]),
+        "https://www.youtube.com/results?search_query=%s" % songSearchTerm
+    ])
+
+jsonFile = open('data.json', "w", encoding="utf8", newline='')
+jsonFile.write("[")
+for i in range(0, len(years)):
+    artist = sanitizeString(artists[i])
+    song = sanitizeString(songs[i])
+    row = Row(
+        int(ids[i]),
+        sanitizeString(genres[i]),
+        artist,
+        song,
+        sanitizeString(years[i]),
+        "https://www.youtube.com/results?search_query=%s" % songSearchTerm
+    )
+    jsonFile.write(json.dumps(row.__dict__))
+    jsonFile.write(",")
+jsonFile.write("]")
