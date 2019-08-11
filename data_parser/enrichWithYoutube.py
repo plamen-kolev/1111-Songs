ab0a68d3a53f93e6bdc3a5a71ae56f454d7e09f8
Plamen Kolev <p.kolev22@gmail.com>
2019-08-11 14:59:01 +0100

    proof of concept for getting metadatra of random song

--- data_parser/enrichWithYoutube.py
+++ data_parser/enrichWithYoutube.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from youtube.songEnricher import enrichSong
 
@@ -7,10 +8,16 @@ with open('./json/data.json') as json_data:
     print(json_data)
     jsonToEnrich = json.load(json_data)
 
-for key in jsonToEnrich:
-    for songIndex in range(0, len(jsonToEnrich[key])):
-        jsonToEnrich[key][songIndex] = enrichSong(jsonToEnrich[key][songIndex])
-        break
-    break
+
+genre, songs = random.choice(list(jsonToEnrich.items()))
+randomSongIndex = random.randint(0,len(songs) - 1)
+print(randomSongIndex)
+
+jsonToEnrich[genre][randomSongIndex] = enrichSong(songs[randomSongIndex])
+# for key in jsonToEnrich:
+#     for songIndex in range(0, len(jsonToEnrich[key])):
+#         jsonToEnrich[key][songIndex] = enrichSong(jsonToEnrich[key][songIndex])
+#         break
+#     break
 with open('json/data.json', 'w') as outfile:
     json.dump(jsonToEnrich, outfile, indent=4)
