44d252419ac0c68324a1a4a10ee9f0a4e89c94cd
Plamen Kolev <p.kolev22@gmail.com>
2019-08-11 20:03:45 +0100

    added more songs and error reporting when something cant be found

--- data_parser/youtube/songEnricher.py
+++ data_parser/youtube/songEnricher.py
@@ -1,12 +1,27 @@
+import json
+
 from youtube.youtubeService import getYoutubePlayInfo
 
 
 def enrichSong(songJson):
     if "enriched" not in songJson:
         print("enriching %s" % songJson.get('song'))
-        dataFromYoutube = getYoutubePlayInfo("%s %s" % (songJson.get('song'), songJson.get('artist')))['items'][0]
+        searchTerm = "%s %s" % (songJson.get('song'), songJson.get('artist'))
+        dataFromYoutube = getYoutubePlayInfo(searchTerm)
+        if not dataFromYoutube:
+            with open('error.json', 'w') as outfile:
+                json.dump("{\"error\": \"something went wrong for song '%s'\"}" % searchTerm, outfile, indent=4)
 
-        songJson['youtube'] = dataFromYoutube
+        itemsFromYoutube = dataFromYoutube['items']
+        if not itemsFromYoutube:
+            with open('error.json', 'w') as outfile:
+                json.dump(itemsFromYoutube, outfile, indent=4)
+        theResult = itemsFromYoutube[0]
+        if not theResult:
+            with open('error.json', 'w') as outfile:
+                json.dump(theResult, outfile, indent=4)
+
+        songJson['youtube'] = theResult
         songJson['enriched'] = True
         return songJson
     else:
