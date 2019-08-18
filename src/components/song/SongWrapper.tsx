bf92d1aeb5e451fb9efbd6d803adf1ee6e0105a1
Plamen Kolev <p.kolev22@gmail.com>
2019-08-18 19:21:25 +0100

    made it so instead of loading 20000 entries json, everything is loaded very lazy, increasing performance drastically

--- src/components/song/SongWrapper.tsx
+++ src/components/song/SongWrapper.tsx
@@ -1,33 +1,63 @@
 import React from 'react';
-import * as data from '../../data.json';
-import { getAllSongs } from '../../utils';
+import dataMap from '../../data/categories_lookup.json';
 import { Grid } from 'semantic-ui-react'
 import { SongLoading } from './SongLoading'
 import { Song } from './Song';
 import LazyLoad from 'react-lazyload';
 import { IframeProps } from 'components/song/Iframe';
 import { JsonSong } from '../../utils'
+import Visibility from "semantic-ui-react/dist/commonjs/behaviors/Visibility";
 
-const songs: JsonSong[] = getAllSongs(data);
 
 type SongWrapperProps = {
     onSongClick(iframeData: IframeProps): void
 }
 
-function areEqual(prevProps: SongWrapperProps, nextProps:SongWrapperProps) {
-    return true;
-}
+const chunksToLoad = 40;
 
-export const SongWrapper = React.memo((props: SongWrapperProps) => {
-    return (
-        <Grid grid className="overflow">
-            {songs.map(song => (
+export class SongWrapper extends React.Component<SongWrapperProps, { songs: JsonSong[] }> {
+    constructor(props: SongWrapperProps, state: any) {
+        super(props, state);
+        this.state = {
+            songs: []
+        };
+    }
+
+    shouldComponentUpdate(nextProps: Readonly<SongWrapperProps>, nextState: Readonly<{ songs: JsonSong[] }>, nextContext: any): boolean {
+        return !(this.state.songs === nextState.songs);
+    }
+
+    componentDidMount(): void {
+        this.addMoreSongs();
+    }
+
+    addMoreSongs = () => {
+        let songs: JsonSong[] = [];
+        for (var i = 0; i < chunksToLoad; i++) {
+            const filename = dataMap.pop();
+            const foo = require(`../../data/categories/${filename}`);
+            songs = songs.concat(foo);
+        }
+
+        const songsToAppend = this.state.songs.concat(songs);
+        this.setState({
+            songs: songsToAppend
+        })
+    };
+    render() {
+        return (
+            <Grid grid className="overflow">
+            {this.state.songs.map((song: JsonSong) => (
                 <Grid.Column key={song.url} mobile={6} tablet={3} computer={3}>
                     <LazyLoad overflow once={true} throttle={100} height={1000} placeholder={<SongLoading/>} >
-                        <Song click={props.onSongClick} {...song}/>
+                        <Song click={this.props.onSongClick} {...song}/>
                     </LazyLoad>
                 </Grid.Column>
             ))}
-        </Grid>
-    )
-}, areEqual);
+                <Visibility
+                    continuous={true}
+                    onBottomVisible={() => this.addMoreSongs()}
+                />
+            </Grid>
+    )}
+}
