2537661213d51ff9862a23a8bd5e7a61db32e70d
Plamen Kolev <p.kolev22@gmail.com>
2019-08-19 06:56:55 +0100

    added google analytic react module instead, changed logos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 import { Iframe } from './components/song/Iframe';
 import { SongWrapper } from './components/song/SongWrapper';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-89618080-1');
 
 function App() {
 
@@ -12,6 +15,8 @@ function App() {
   const onSongClick = (song) => {
     setState({currentSong: song})
   }
+
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <div className="App container">
       <div className="ui centered grid iframe-container">
