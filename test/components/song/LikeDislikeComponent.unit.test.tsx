89b76c2a545c6692a0db4d4e2bc136775ab2fd47
Plamen Kolev <p.kolev22@gmail.com>
2019-08-25 21:54:10 +0100

    SGN-10: moved like/dislike component to its own module with tests

--- test/components/song/LikeDislikeComponent.unit.test.tsx
+++ test/components/song/LikeDislikeComponent.unit.test.tsx
@@ -3,20 +3,20 @@ import {LikeDislikeComponent} from "../../../src/components/song/LikeDislikeComp
 import React from "react";
 import '@testing-library/jest-dom/extend-expect'
 
+beforeEach(() => {
+    cleanup();
+});
+
 describe("LikeDislike component", () => {
+    let mockSetLiked = jest.fn();
+    let getByTestId: any;
+    let debug;
 
     describe("when interacting for the first time", () => {
-        let mockSetLiked = jest.fn();
-        let getByText;
-        let getByTestId: any;
-        let debug;
-
         beforeEach(() => {
-            cleanup();
             const renderer = render(
                 <LikeDislikeComponent liked={undefined} unique_id={"unique"} setLiked={mockSetLiked} />
             );
-            getByText = renderer.getByText;
             getByTestId = renderer.getByTestId;
             debug = renderer.debug;
         });
@@ -36,5 +36,20 @@ describe("LikeDislike component", () => {
             fireEvent.click(getByTestId("dislike-button"));
             expect(mockSetLiked).toHaveBeenCalledWith(false);
         });
+    });
+
+
+    it("when liked green button should be selected",() => {
+        const {getByTestId} = render(
+            <LikeDislikeComponent liked={true} unique_id={"unique"} setLiked={mockSetLiked} />
+        );
+        expect(getByTestId("like-button")).not.toHaveClass("basic");
+    });
+
+    it("when liked red button should be selected",() => {
+        const {getByTestId} = render(
+            <LikeDislikeComponent liked={false} unique_id={"unique"} setLiked={mockSetLiked} />
+        );
+        expect(getByTestId("dislike-button")).not.toHaveClass("basic");
     });
 });
