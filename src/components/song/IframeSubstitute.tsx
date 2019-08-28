1f9cc036c4d74205ab52b712e16d7f8015647a41
Plamen Kolev <p.kolev22@gmail.com>
2019-08-28 07:33:48 +0100

    fixed zindex bug with the loading component. Changed the design of iframe when no video is being played Added rest of  the songs

--- src/components/song/IframeSubstitute.tsx
+++ src/components/song/IframeSubstitute.tsx
@@ -13,14 +13,13 @@ interface IIframeSubstituteProps {
 
 export const IframeSubstitute = ({url, title}: IIframeSubstituteProps) => {
     return (
-        <div className="fake-iframe-music-player">
-            { url && <Button className="iframe-music-player-button" onClick={() => openInNewTab(url)} variant="contained">
-                <p>Open "{title}" on youtube</p>
-            </Button>}
-
-            {! url && <Button className="fake-iframe-music-player-button" disabled variant="contained">
-                <p>Click a song</p>
-            </Button>}
-
-        </div>);
+        <>
+            { url &&
+            <div className="fake-iframe-music-player">
+                <Button className="iframe-music-player-button" onClick={() => openInNewTab(url)} variant="contained">
+                    <p>Open "{title}" on youtube</p>
+                </Button>
+            </div>}
+        </>
+    );
 };
